cf35e6e17880f837a4fff09881b8559b8ca37ab4
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-05-02 10:17:06 +0900

    Fix attaching files to the message

--- src/patch/APIMessage.ts
+++ src/patch/APIMessage.ts
@@ -7,6 +7,8 @@ const original = APIMessage.prototype.resolveData;
 class PatchAPIMessage extends APIMessage {
     public resolveData(): this {
         const token = 'DONtTRyTOSteALThETokEn0k.PLeasE.YOuHaVEBeENWARnedD0N0TuSEIT';
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const cache = Object.create(this.options);
         let options = JSON.stringify(this.options);
 
         // This is the last resort to stop leaking the token
@@ -15,6 +17,10 @@ class PatchAPIMessage extends APIMessage {
             options = options.replaceAll(t, token.split('.')[splitedToken.indexOf(t)]);
         }
         this.options = JSON.parse(options.replaceAll(instance.token ?? '', token)) as MessageOptions | WebhookMessageOptions;
+
+        // TODO: Check for file strings
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+        this.options.files = cache.files;
 
         original.call(this);
 
