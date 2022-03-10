63c60489c91d651adb14979721ee667e36aa9804
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-03-10 16:52:35 +0900

    Update DJSDocument interface

--- src/interface/DJSDocument.ts
+++ src/interface/DJSDocument.ts
@@ -1,24 +1,168 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention
+/* eslint-disable @typescript-eslint/naming-convention */
+
 export interface DJSDocument {
-    classes: {
+    meta: {
+        generator: string;
+        format: number;
+        date: number;
+    };
+    custom: {
+        general: {
+            name: string;
+            files: {
+                welcome: {
+                    name: string;
+                    type: string;
+                    content: string;
+                    path: string;
+                };
+            };
+        };
+    };
+    classes: Class[];
+    interfaces: Interface[];
+    typedefs: Typedef[];
+    externals: {
+        name: string;
+        see: string[];
+        meta: Meta;
+    }[];
+}
+
+interface Class {
+    name: string;
+    description: string;
+    extends?: string[][][];
+    props?: {
+        name: string;
+        description?: string;
+        type: string[][][];
+        meta: Meta;
+        access?: 'private';
+        readonly?: true;
+        nullable?: true;
+        abstract?: boolean;
+        see?: string[];
+        deprecated?: boolean | string;
+        props?: PropElement[];
+        scope?: 'static';
+    }[];
+    methods?: {
         name: string;
         description: string;
-        internal_type: 'class';
-        props?: string[];
-        methods?: string[];
-        events?: string[];
+        returns?: TentacledReturns;
+        meta: Meta;
+        access?: 'private';
+        examples?: string[];
+        params?: ParamElement[];
+        async?: boolean;
+        inherits?: string;
+        inherited?: boolean;
+        implements?: string[];
+        see?: string[];
+        scope?: 'static';
+        emits?: string[];
+        deprecated?: string;
+        abstract?: boolean;
     }[];
-    typedefs: {
+    events?: {
         name: string;
         description: string;
-        internal_type: 'typedef';
-        type: string;
+        params?: Params[];
+        meta: Meta;
+        deprecated?: string;
     }[];
-    interfaces: {
+    meta: Meta;
+    construct?: {
+        name: string;
+        params: PropElement[];
+    };
+    implements?: string[][][];
+    abstract?: boolean;
+    access?: 'private';
+}
+
+interface Interface {
+    name: string;
+    description: string;
+    methods: {
         name: string;
         description: string;
-        internal_type: 'typedef';
-        props?: string[];
-        methods: string[];
+        examples?: string[];
+        params?: PropElement[];
+        async?: boolean;
+        returns: string[][][] | Returns;
+        meta: Meta;
+        see?: string[];
     }[];
+    meta: Meta;
+    props?: {
+        name: string;
+        description: string;
+        type: string[][][];
+        meta: Meta;
+        nullable?: true;
+        readonly?: true;
+    }[];
+}
+
+interface Typedef {
+    name: string;
+    description?: string;
+    type: string[][][];
+    props?: ParamElement[];
+    meta: Meta;
+    see?: string[];
+    params?: Params[];
+    returns?: string[][][] | Returns;
+    access?: 'private';
+}
+
+interface PropElement {
+    name: string;
+    description: string;
+    type: string[][][];
+    optional?: boolean;
+    default?: unknown;
+    nullable?: true;
+}
+
+interface Meta {
+    line: number;
+    file: string;
+    path: string;
+}
+
+interface Params {
+    name: string;
+    description: string;
+    type: TypeElement[][];
+    nullable?: true;
+    variable?: boolean;
+}
+
+type TypeElement = string[] | '*';
+
+
+interface ParamElement {
+    name: string;
+    description: string;
+    optional?: boolean;
+    default?: FluffyDefault;
+    type: TypeElement[][];
+    nullable?: true;
+    variable?: boolean;
+}
+
+type FluffyDefault = boolean | number | null | string;
+
+type TentacledReturns = TypeElement[][] | {
+    types: TypeElement[][];
+    description?: string;
+    nullable?: true;
+};
+
+interface Returns {
+    types: string[][][];
+    description: string;
 }
