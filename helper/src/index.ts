b2d0156af31c85422f2e18c09f0869f852aee43c
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-09-12 13:07:37 +0900

    check

--- helper/src/index.ts
+++ helper/src/index.ts
@@ -3,10 +3,29 @@ import { platform } from 'process';
 
 if (platform === 'win32') throw new Error('This script is not supported on Windows');
 
+const parentPid: string[] = [];
+
 setInterval(() => {
     console.log('----------------------------------------');
     spawn('ps', ['aux', '-o', 'pid,etime,comm,args'])
         .stdout.setEncoding('utf8')
-        .on('data', console.log);
+        .on('data', (data: string) => {
+            const process = data
+                .split('\n')
+                .map(s => s.trim())
+                .filter((v, i) => i > 0 && v)
+                .map(line => {
+                    const [pid, time, command, ...args] = line.split(/\s+/);
+                    return { pid, time, command, args: args.slice(1) };
+                });
+            if (parentPid.length === 0) {
+                process
+                    .filter(({ command }) => command === 'node')
+                    .map(({ pid }) => pid)
+                    .filter((pid): pid is string => typeof pid === 'string')
+                    .forEach(pid => parentPid.push(pid));
+            }
+            console.log(process, parentPid);
+        });
     console.log('----------------------------------------');
 }, 5000);
