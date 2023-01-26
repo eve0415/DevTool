cd7369464956de58f9dd867faba0b233abea1817
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-01-26 12:20:27 +0900

    build: fix build script

--- build.js
+++ build.js
@@ -2,22 +2,53 @@
 
 // @ts-check
 
-/* eslint-disable import/no-extraneous-dependencies */
-
+import { error, log } from 'console';
+import { context } from 'esbuild';
 import { join } from 'path';
-import { cwd } from 'process';
-import { build } from 'esbuild';
+import prettyBytes from 'pretty-bytes';
+import { argv, cwd, exit } from 'process';
 
-build({
-    entryPoints: [join(cwd(), 'src', 'index.ts')],
-    outdir: join(cwd(), 'out'),
-    bundle: true,
-    minify: true,
-    sourcesContent: false,
-    allowOverwrite: true,
-    format: 'esm',
-    platform: 'node',
-    sourcemap: 'inline',
-    tsconfig: join(cwd(), 'tsconfig.json'),
-    packages: 'external',
+const ctx = await context({
+  entryPoints: [join(cwd(), 'src', 'index.ts')],
+  outdir: join(cwd(), 'out'),
+  bundle: true,
+  minify: true,
+  sourcesContent: false,
+  allowOverwrite: true,
+  format: 'esm',
+  platform: 'node',
+  sourcemap: 'inline',
+  tsconfig: join(cwd(), 'tsconfig.json'),
+  packages: 'external',
+  metafile: true,
+  plugins: [
+    {
+      name: 'bundle-size',
+      setup: ({ onEnd }) => {
+        onEnd(({ errors, metafile }) => {
+          if (errors.length) {
+            error(errors);
+            return;
+          }
+
+          const size = Object.values(metafile?.outputs ?? {}).reduce(
+            (a, b) => a + b.bytes,
+            0
+          );
+          log(`Build complete with ${prettyBytes(size, { locale: 'ja' })}`);
+        });
+      },
+    },
+  ],
 });
+
+process.on('beforeExit', () => {
+  ctx.dispose().catch(error);
+});
+
+if (!argv.includes('--watch')) {
+  await ctx.rebuild();
+  exit();
+}
+
+await ctx.watch();
