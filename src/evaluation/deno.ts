9bb815387d4e2a9a67311894ccbcf1c795a39928
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-01-30 18:16:00 +0900

    fix: should work now

--- src/evaluation/deno.ts
+++ src/evaluation/deno.ts
@@ -5,4 +5,8 @@ export class DenoEvaluationSystem extends BaseEvaluationSystem {
   public override evaluate(content: string): Promise<BaseMessageOptions> {
     return super.evaluate(`${content}\n\nclose()\n`, 'deno', ['repl']);
   }
+
+  protected override processContent(content: unknown[]): string[] {
+    return super.processContent(content).slice(1);
+  }
 }
